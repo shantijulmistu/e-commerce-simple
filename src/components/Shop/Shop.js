087d93e05eb64256fcc4685d36b1e77a4b967e4c
shantijulislam <shantijulislam6776@gmail.com>
2022-11-11 03:34:43 +0600

    e-commerce-simple is done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -16,16 +16,32 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getStoredCart();
+        const savedCart = [];
         for (const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
-            console.log(addedProduct)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
         }
-    }, [])
+        setCart(savedCart);
+    }, [products])
 
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = [...rest, exists];
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className="shop-container">
